b7c2088059e1d81547e14ff3326fd41d6451e1c3
lleonardus <leonardus.wizard@gmail.com>
2025-07-23 18:28:13 -0300

    exporting result to a csv file

--- script.py
+++ script.py
@@ -1,28 +1,31 @@
+import csv
 import nmap
 import netifaces
 
 # TODO: Automatizar escolha de interface, ou pelo menos colocar isso como um parâmetro
-# TODO: Descobrir como transformar o output em um arquivo csv
 
+addrs = netifaces.ifaddresses("enp2s0")
+host_ip = addrs[netifaces.AF_INET][0]["addr"]  # Ex: 192.168.0.25
+nm = nmap.PortScanner()
+nm.scan(
+    hosts=f"{host_ip}/24", arguments="-sn", sudo=True
+)  # Ping Scan (disable port scan)
 
-def get_info():
-    addrs = netifaces.ifaddresses("enp2s0")
-    host_ip = addrs[netifaces.AF_INET][0]["addr"]  # Ex: 192.168.0.25
-    nm = nmap.PortScanner()
-    nm.scan(
-        hosts=f"{host_ip}/24", arguments="-sn", sudo=True
-    )  # Ping Scan (disable port scan)
-
-    print("IP;MAC;Fabricante")
+with open("relatorio.csv", "w", newline="") as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=";")
+    spamwriter.writerow(["IP", "MAC", "Fabricante"])
     for host in nm.all_hosts():
         if nm[host]["status"]["reason"] != "localhost-response":
             mac = nm[host]["addresses"]["mac"]
             fabricante = list(nm[host]["vendor"].values())
-            print(f"{host};{mac};{fabricante[0] if len(fabricante) > 0 else None}")
+            spamwriter.writerow(
+                [host, mac, fabricante[0] if len(fabricante) > 0 else "null"]
+            )
         else:
-            mac = addrs[netifaces.AF_LINK][0]["addr"]
+            mac = addrs[netifaces.AF_LINK][0]["addr"].upper()
             fabricante = list(nm[host]["vendor"].values())
-            print(f"{host};{mac};{fabricante[0] if len(fabricante) > 0 else None}")
+            spamwriter.writerow(
+                [host, mac, fabricante[0] if len(fabricante) > 0 else "null"]
+            )
 
-
-get_info()
+print("Relatório concluído e salvo em relatorio.csv")
